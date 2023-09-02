99440c394e80890ed8b184e81e3f663f1427f977
yunyuuuu <105867559+yunyuuuu@users.noreply.github.com>
2023-09-02 11:57:07 +0800

    Update preload-image.js

--- js/preload-image.js
+++ js/preload-image.js
@@ -4,8 +4,16 @@ export default function(people){
     let i = 0;
 
     const next = () => {
-        const url = people[i].digit_album_img;
-        loadImage(url, () => {
+        const url = `./img/entity/${people[i].index}.webp`;
+        const url2 = `./img/digit/${people[i].index}.webp`;
+        if (people[i].enitity == 1){
+            loadImage(url, () => {
+                // i += 1;
+                if (i < people.length) next();
+                else console.log('done preloading images');
+            })
+        }
+        loadImage(url2, () => {
             i += 1;
             if (i < people.length) next();
             else console.log('done preloading images');
